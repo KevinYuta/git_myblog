f8951cd2cd98a0baddfae3d49a7f5de6947b26b1
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-06-12 08:26:20 +0000

    commentコントローラーのcreateアクションの定義

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -2,5 +2,12 @@ class CommentController < ApplicationController
     def new
     end
     
-    
+    def create
+    Comment.create(comment_params)
+    end
+
+    private
+    def comment_params
+    params.permit(:content, :author, :email, :article_id)
+    end
 end
