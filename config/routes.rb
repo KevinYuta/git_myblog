c6ea8eaad190a174faac9fda83093aa90b94c777
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-06-12 08:44:24 +0000

    パンくずの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  root    'article#index'
   resources :articles, only: [:index,:show] do
     resources :comments, only: [:create,:new]
   end
