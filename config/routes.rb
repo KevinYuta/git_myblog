1e5f1537cee9fd55236b7fc8766a16a10d1258d2
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-06-12 08:15:57 +0000

    articleコントローラーのshowアクションの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  resources :articles do
+  resources :articles, only: [:index,:show] do
     resources :comments, only: [:create,:new]
   end
 end
