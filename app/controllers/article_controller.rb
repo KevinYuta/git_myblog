516902e34471753bc9790cdf25727fac2b81a9bd
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-06-12 08:19:33 +0000

    articleコントローラーのindexアクションとshowアクションの修正

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,7 +1,9 @@
 class ArticleController < ApplicationController
     def index
+        @article = Article.page(params[:page]).per(5).order("created_at DESC")
     end
     
     def show
+        @article = Article.find(params[:id])
     end
 end
