61b65705aca2905c5becf681a469184ae17e5bb7
EC2 Default User <ec2-user@ip-172-31-31-43.ap-southeast-1.compute.internal>
2019-06-15 09:35:10 +0000

    readmeの記述と軽微な修正

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,9 +1,9 @@
 class ArticleController < ApplicationController
     def index
-        @article = Article.page(params[:page]).per(5).order("created_at DESC")
+        @articles = Article.page(params[:page]).per(10).order("created_at DESC")
     end
     
     def show
-        @article = Article.find(params[:id])
+        @articles = Article.find(params[:id])
     end
 end
